cb442fe42b530efc9f413ce4836fec1d76265832
krittika-das <Krittika.Das@gds.ey.com>
2025-11-25 08:57:37 +0000

    Added set files

---
+++ 25-11-2025/PythonProject1/set_basics.py
@@ -0,0 +1,22 @@
+nums={10, 20, 30, 40}
+
+#how to create a set
+data={1, 2, 3, 3, 3, 4, 3, 5, 2}
+print(data)
+
+s={10, 20, 30}
+empty=set()
+
+#set operations
+s={1, 2, 3}
+s.add(4)
+s.update([5])
+
+print(s)
+
+#s.remove(8)
+
+s.discard(9)
+
+s.pop()
+print(s)
