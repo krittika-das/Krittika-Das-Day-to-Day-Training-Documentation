ca5edb40d529019246844d5ffe65787ad9dc6174
krittika-das <Krittika.Das@gds.ey.com>
2025-12-03 11:33:03 +0530

    Add files via upload

---
+++ 03-12-2025/table_joins.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE Company;
+use  Company;
+create table departments(dept_id int primary key, dept_name varchar(50), location varchar(50));
+create table employees(emp_id int primary key, emp_name varchar(50), email varchar(80), salary int, dept_id int null);
+INSERT INTO departments (dept_id, dept_name, location)
+VALUES
+(10, 'HR', 'Mumbai'),
+(20, 'Finance', 'Delhi'),
+(30, 'IT', 'Bangalore'),
+(40, 'Marketing', 'Chennai'),
+(50, 'Admin', 'Hyderabad');   -- no employees for dept 50
+
+INSERT INTO employees (emp_id, emp_name, email, salary, dept_id)
+VALUES
+(101, 'Aisha Khan', 'aisha@company.com', 60000, 10),
+(102, 'Rahul Sharma', 'rahul@company.com', 72000, 20),
+(103, 'Meera Iyer', 'meera@company.com', 50000, 30),
+(104, 'Imran Ali', 'imran@company.com', 65000, NULL),  -- no department
+(105, 'Sanjay Patel', 'sanjay@company.com', 45000, NULL), -- no department
+(106, 'David John', 'david@company.com', 90000, 40);
+
+select e.emp_id, e.emp_name, d.dept_name, d.location from employees e inner join departments d on d.dept_id=e.dept_id;
+
+select e.emp_id, e.emp_name, d.dept_name from employees e left join departments d on d.dept_id=e.dept_id;
+
+select e.emp_id, e.emp_name, d.dept_name from employees e right join departments d on d.dept_id=e.dept_id;
+
+-- Full Join
+select e.emp_id, e.emp_name, d.dept_name from employees e left join departments d on d.dept_id=e.dept_id
+union
+select e.emp_id, e.emp_name, d.dept_name from employees e right join departments d on d.dept_id=e.dept_id
