5cdc43e7f58bff9e5db0776b812ff551bd2d40b7
krittika-das <Krittika.Das@gds.ey.com>
2025-12-04 15:32:05 +0530

    Add files via upload

---
+++ 04-12-2025/employees_python_sql_db_connector/python_employees_crud.py
@@ -0,0 +1,38 @@
+import mysql.connector
+
+conn=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="Memories000%",
+    database="company_db"
+)
+
+cursor = conn.cursor()
+
+def addemp(name, dept, salary):
+    query = "INSERT INTO employess (name, dept, salary) VALUES (%s, %s, %s)"
+    data=(name, dept, salary)
+    cursor.execute(query, data)
+    conn.commit()
+
+def viewall():
+    query = "SELECT * FROM employess"
+    cursor.execute(query)
+    for row in cursor.fetchall():
+        print(row)
+
+def update_salary(empid, new_salary):
+    query = "UPDATE employess SET salary = %s WHERE empid = %s"
+    cursor.execute(query, (new_salary, empid))
+    conn.commit()
+
+addemp('Krittika', 'Advanced Engineering', 35000)
+addemp('Priyanka', 'SAP', 32900)
+addemp('Madhusmita', 'Cloud', 12000)
+
+viewall()
+
+update_salary('1', 54000)
+
+viewall()
+
