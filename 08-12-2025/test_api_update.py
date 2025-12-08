afe56e992cca828b989276d9ae554746aff6ea1e
krittika-das <Krittika.Das@gds.ey.com>
2025-12-08 11:08:43 +0530

    Add files via upload

---
+++ 08-12-2025/test_api_update.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+items=["Apple", "Banana", "Orange"]
+
+@app.route("/items", methods=["GET"])
+def get_items():
+    return jsonify(items)
+
+@app.route("/items", methods=["POST"])
+def add_item():
+    data = request.get_json()
+    item=data.get("item")
+    items.append(item)
+    return "POST EXECUTED"
+
+@app.route("/items/<int:index>", methods=["PUT"])
+def update_item(index):
+    data = request.get_json()
+    nv=data.get("item")
+    items[index]=nv
+    return "PUT EXECUTED"
+
+if __name__ == "__main__":
+    app.run(debug=True)
