a0ff4ccfc8f187dd602c04a3f0fd7db8cb819315
krittika-das <Krittika.Das@gds.ey.com>
2025-12-11 16:41:03 +0530

    Add files via upload

---
+++ 11-12-2025/Django_todolist/todo_project/tasks/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all().order_by('-created_at')
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        Task.objects.create(title=title, description=description)
+        return redirect('task_list')
+    return render(request, 'tasks/add_task.html')
+
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        task.title = request.POST.get('title')
+        task.description = request.POST.get('description')
+        task.save()
+        return redirect('task_list')
+    return render(request, 'tasks/edit_task.html', {'task': task})
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.status = 'Completed'
+    task.save()
+    return redirect('task_list')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('task_list')
